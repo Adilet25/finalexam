158b86b01c067d3eade5cef725483499e40896e4
Adilet <baryktabasovadi@gmail.com>
2023-12-09 23:38:23 +0600

    created footer

--- src/MainLayout/footer/Footer.tsx
+++ src/MainLayout/footer/Footer.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import "../../styles/Footer.scss";
 
 type Props = {};
 
 const Footer = (props: Props) => {
-  return <div>Footer</div>;
+  return (
+    <div className="footerBlock">
+      <h1>JOB KG</h1>
+      <div>
+        <p>Для соискателя</p>
+        <p>Для работодателя</p>
+        <p>Инвесторам</p>
+      </div>
+      <div>
+        <p>О сервисе </p>
+        <p>Помощь</p>
+        <p>Служба поддержки</p>
+      </div>
+      <div>
+        <p>+996 999 661 777</p>
+        <p>+996 709 831 050</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
